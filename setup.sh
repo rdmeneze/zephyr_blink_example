32c352689c3a39e94ec812caade6e8d1747540d0
rdmeneze <rdmeneze@gmail.com>
2020-08-28 14:35:50 +0200

    - changed the zephyr-sdk path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export ZEPHYR_BASE="$(pwd)/zephyr"
 export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
-export ZEPHYR_SDK_INSTALL_DIR=/opt/zephyr-sdk
+export ZEPHYR_SDK_INSTALL_DIR=/home/rdias/zephyr-sdk-0.11.3
 source $ZEPHYR_BASE/zephyr-env.sh
